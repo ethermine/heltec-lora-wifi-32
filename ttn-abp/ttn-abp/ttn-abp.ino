eef63815a6514f4c3d42ee7962d4b1ea5fa71f66
FAYARD Clément <cfa@technord.com>
2018-03-27 19:04:14 +0200

    .

--- ttn-abp/ttn-abp/ttn-abp.ino
+++ ttn-abp/ttn-abp/ttn-abp.ino
@@ -11,57 +11,47 @@
  * world!", using frequency and encryption settings matching those of
  * the The Things Network.
  *
- * This uses OTAA (Over-the-air activation), where where a DevEUI and
- * application key is configured, which are used in an over-the-air
- * activation procedure where a DevAddr and session keys are
- * assigned/generated for use with all further communication.
+ * This uses ABP (Activation-by-personalisation), where a DevAddr and
+ * Session keys are preconfigured (unlike OTAA, where a DevEUI and
+ * application key is configured, while the DevAddr and session keys are
+ * assigned/generated in the over-the-air-activation procedure).
  *
  * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
  * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
  * violated by this sketch when left running for longer)!
-
+ *
  * To use this sketch, first register your application and device with
- * the things network, to set or generate an AppEUI, DevEUI and AppKey.
- * Multiple devices can use the same AppEUI, but each device has its own
- * DevEUI and AppKey.
+ * the things network, to set or generate a DevAddr, NwkSKey and
+ * AppSKey. Each device should have their own unique values for these
+ * fields.
  *
  * Do not forget to define the radio type correctly in config.h.
  *
  *******************************************************************************/
+
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
-#include <Wire.h>  
-#include "SSD1306.h" 
 
-SSD1306 display(0x3c, 4, 15);
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const PROGMEM u1_t NWKSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
 
-#define BUILTIN_LED 25
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const u1_t PROGMEM APPSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
 
-osjob_t initjob;
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x26001EB8 ; // <-- Change this address for every node!
 
-// This EUI must be in little-endian format, so least-significant-byte
-// first. When copying an EUI from ttnctl output, this means to reverse
-// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
-// 0x70.
-// 70B3D57ED000B2AD
-// 70 B3 D5 7E D0 00 B2 AD
-static const u1_t PROGMEM APPEUI[8]={ 0xAD , 0xB2, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
-void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
-
-// This should also be in little endian format, see above.
-// EF08457EB0005511
-// EF 08 45 7E B0 00 55 11
-static const u1_t PROGMEM DEVEUI[8]={ 0x11, 0x55, 0x00, 0xB0, 0x7E, 0x45, 0x08, 0xEF };
-void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
-
-// This key should be in big endian format (or, since it is not really a
-// number but a block of memory, endianness does not really apply). In
-// practice, a key taken from ttnctl can be copied as-is.
-// The key shown here is the semtech default key.
-// 2B7E152628AED2A6ABF7158809CF4F3C
-static const u1_t PROGMEM APPKEY[16] = { 0x2B, 0x7E, 0x15, 0x26, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
 
 static uint8_t mydata[] = "Hello, world!";
 static osjob_t sendjob;
@@ -69,7 +59,6 @@ static osjob_t sendjob;
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
 const unsigned TX_INTERVAL = 60;
-
 #define SS 18 // ESP32 GPIO18 (Pin18) – SX1276 NSS (Pin19) SPI Chip Select Input
 #define MOSI 27 // ESP32 GPIO27 (Pin27) – SX1276 MOSI (Pin18) SPI Data Input
 #define MISO 19 // ESP32 GPIO19 (Pin19) – SX1276 MISO (Pin17) SPI Data Output
@@ -88,71 +77,40 @@ const lmic_pinmap lmic_pins = {
 };
 
 void onEvent (ev_t ev) {
-    Serial.print(ev);
-    Serial.print(" / ");
     Serial.print(os_getTime());
     Serial.print(": ");
-    display.clear();
-    display.drawString(0, 20, "DevEUI : EF08457EB0005511");
-    display.display();
-    
     switch(ev) {
         case EV_SCAN_TIMEOUT:
             Serial.println(F("EV_SCAN_TIMEOUT"));
-            display.drawString(0, 0, "EV_SCAN_TIMEOUT");
-            display.display();
             break;
         case EV_BEACON_FOUND:
             Serial.println(F("EV_BEACON_FOUND"));
-            display.drawString(0, 0, "EV_BEACON_FOUND");
-            display.display();
             break;
         case EV_BEACON_MISSED:
             Serial.println(F("EV_BEACON_MISSED"));
-            display.drawString(0, 0, "EV_BEACON_MISSED");
-            display.display();
             break;
         case EV_BEACON_TRACKED:
             Serial.println(F("EV_BEACON_TRACKED"));
-            display.drawString(0, 0, "EV_BEACON_TRACKED");
-            display.display();
             break;
         case EV_JOINING:
             Serial.println(F("EV_JOINING"));
-            display.drawString(0, 0, "EV_JOINING");
-            display.display();
             break;
         case EV_JOINED:
             Serial.println(F("EV_JOINED"));
-            display.drawString(0, 0, "EV_JOINED");
-            display.display();
-            // Disable link check validation (automatically enabled
-            // during join, but not supported by TTN at this time).
-            LMIC_setLinkCheckMode(0);
             break;
         case EV_RFU1:
             Serial.println(F("EV_RFU1"));
-            display.drawString(0, 0, "EV_RFU1");
-            display.display();
             break;
         case EV_JOIN_FAILED:
             Serial.println(F("EV_JOIN_FAILED"));
-            display.drawString(0, 0, "EV_JOIN_FAILED");
-            display.display();
             break;
         case EV_REJOIN_FAILED:
             Serial.println(F("EV_REJOIN_FAILED"));
-            display.drawString(0, 0, "EV_REJOIN_FAILED");
-            display.display();
-            break;
             break;
         case EV_TXCOMPLETE:
             Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            display.drawString(0, 0, "EV_TXCOMPLETE");
-            display.display();
-            if (LMIC.txrxFlags & TXRX_ACK){
+            if (LMIC.txrxFlags & TXRX_ACK)
               Serial.println(F("Received ack"));
-            }
             if (LMIC.dataLen) {
               Serial.println(F("Received "));
               Serial.println(LMIC.dataLen);
@@ -163,66 +121,41 @@ void onEvent (ev_t ev) {
             break;
         case EV_LOST_TSYNC:
             Serial.println(F("EV_LOST_TSYNC"));
-            display.drawString(0, 0, "EV_LOST_TSYNC");
-            display.display();
             break;
         case EV_RESET:
             Serial.println(F("EV_RESET"));
-            display.drawString(0, 0, "EV_RESET");
-            display.display();
             break;
         case EV_RXCOMPLETE:
             // data received in ping slot
             Serial.println(F("EV_RXCOMPLETE"));
-            display.drawString(0, 0, "EV_RXCOMPLETE");
-            display.display();
             break;
         case EV_LINK_DEAD:
             Serial.println(F("EV_LINK_DEAD"));
-            display.drawString(0, 0, "EV_LINK_DEAD");
-            display.display();
             break;
         case EV_LINK_ALIVE:
             Serial.println(F("EV_LINK_ALIVE"));
-            display.drawString(0, 0, "EV_LINK_ALIVE");
-            display.display();
             break;
          default:
             Serial.println(F("Unknown event"));
-            display.drawString(0, 0, "Unknown event");
-            display.display();
             break;
     }
 }
 
 void do_send(osjob_t* j){
-    display.clear();
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
         Serial.println(F("OP_TXRXPEND, not sending"));
-        display.drawString(0, 0, "OP_TXRXPEND, not sending");
-        display.display();
     } else {
         // Prepare upstream data transmission at the next possible time.
         LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
         Serial.println(F("Packet queued"));
-        display.drawString(0, 0, "Packet queued");
-        display.display();
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(9200);
     Serial.println(F("Starting"));
-    pinMode(16,OUTPUT);
-    digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
-    delay(50); 
-    digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
-    display.init();
-    display.flipScreenVertically();  
-    display.setFont(ArialMT_Plain_10);
-    display.clear();
 
     #ifdef VCC_ENABLE
     // For Pinoccio Scout boards
@@ -230,27 +163,72 @@ void setup() {
     digitalWrite(VCC_ENABLE, HIGH);
     delay(1000);
     #endif
-    SPI.begin(5, 19, 27);
+
     // LMIC init
     os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
 
-    os_setCallback(&initjob, initfunc);
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    #if defined(CFG_eu868)
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+    // NA-US channels 0-71 are configured automatically
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868100000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+    #elif defined(CFG_us915)
+    // NA-US channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    LMIC_selectSubBand(1);
+    #endif
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    // Start job
+    do_send(&sendjob);
 }
 
 void loop() {
-    os_runloop();
+    os_runloop_once();
 }
-
-// initial job 
-static void initfunc (osjob_t* j) { 
-  Serial.println(F("Init func"));
-  // reset MAC state 
-  LMIC_reset(); 
-  LMIC.dn2Dr = DR_SF7;
-  LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 1000);
-  
-  // start joining 
-  LMIC_startJoining(); 
-  // init done - onEvent() callback will be invoked... 
-}
-
